3251add6486646a23498430506e806d3d1230af0
D <deamonswag@web.de>
2024-06-28 06:34:14 +0200

    +

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'sql204.infinityfree.com', // z.B. sql123.example.com
-    user: 'if0_36803916',
-    password: 'p0oSLfocyMXc',
-    database: 'if0_36803916_scriptdb'
+    host: 'sqlserver', // z.B. sql123.example.com
+    user: 'sqluser',
+    password: 'sqlpw',
+    database: 'sqldbname'
 });
 
 db.connect(err => {
